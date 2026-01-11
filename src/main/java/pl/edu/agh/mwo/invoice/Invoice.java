803975878cfbab1294dad41ce3e8a82689f24f65
Tomasz Wincencik <wincenci@d17-426a-pc-15.ipa.ii.agh.edu.pl>
2026-01-11 15:56:45 +0100

    Status from lesson

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,56 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
+    //private Collection<Product> products = new ArrayList<>();
+    //private Product product;
+
+    private Map<Product, Integer> products =
+            new HashMap<>();
+
 
     public void addProduct(Product product) {
-        // TODO: implement
+
+        this.addProduct(product, 1);
+    }
+    public void addProduct(Product product, Integer quantity) {
+        this.products.put(product, quantity);
     }
 
-    public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
-    }
 
     public BigDecimal getSubtotal() {
-        return null;
+        BigDecimal value = BigDecimal.ZERO;
+        for (Product product : this.products.keySet()) {
+            Integer quantity = this.products.get(product);
+            BigDecimal price = product.getPrice();
+            price = price.multiply(BigDecimal.valueOf(quantity));
+            value = value.add(price);
+        }
+        return value;
+
     }
 
     public BigDecimal getTax() {
-        return null;
+
+        return getTotal().subtract(getSubtotal());
+
     }
 
     public BigDecimal getTotal() {
-        return null;
+        BigDecimal value = BigDecimal.ZERO;
+        for (Product product : this.products.keySet()) {
+            Integer quantity = this.products.get(product);
+            BigDecimal price = product.getPriceWithTax().subtract(product.getPrice());
+            price = price.multiply(BigDecimal.valueOf(quantity));
+            value = value.add(price);
+        }
+        return value;
     }
 }
